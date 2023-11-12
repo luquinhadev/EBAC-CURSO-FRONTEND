39711c002a8a6ddac14a7f0922cd21c51bbf17d2
Erderson Lucas <erdersonlucas05@gmail.com>
2023-11-12 16:07:23 -0300

    Finalizando exercicio 25

---
+++ exercicio25.js
@@ -0,0 +1,25 @@
+const alunos = [
+    {
+        nome: 'João', nota: 7
+    },
+    {
+        nome: 'Lucas', nota: 8
+    },
+    {
+        nome: 'Miguel', nota: 5
+    },
+    {
+        nome: 'Erderson', nota: 6
+    },
+    {
+        nome: 'Gian', nota: 4
+    },
+];
+
+function alunosAprovados(arrayDeAlunos) {
+    return arrayDeAlunos.filter(aluno => aluno.nota >= 6)
+
+};
+
+const alunosAprovadosArray = alunosAprovados(alunos);
+console.log(alunosAprovadosArray);
