a2bf8fd1eb22a483ab521ed63ae07360e808f604
Erderson Lucas <erdersonlucas05@gmail.com>
2023-11-20 18:43:31 -0300

    finalizando exercicio ts

---
+++ exercicioTypeScript/exercicio.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(x, y) {
+    return x * y;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(7, 7);
+const mensagemSaudacao = saudacao("João");
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+console.log("Mensagem de saudação:", mensagemSaudacao);
---
+++ exercicioTypeScript/exercicio.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(x: number, y: number): number {
+    return x * y;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(7, 7);
+const mensagemSaudacao = saudacao("João");
+
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+console.log("Mensagem de saudação:", mensagemSaudacao);
